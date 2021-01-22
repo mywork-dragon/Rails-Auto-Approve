32a17e0cbba9e9fe564c4a95694f100c11e4964b
mywork-dragon <dragonblueyounger@gmail.com>
2021-01-22 11:45:42 +0000

    Add custom.js file. (Navbar is fixed on top now)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import "stylesheets/application"
 
 global.$ = require("jquery")
 
+import "../scr/custom"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
