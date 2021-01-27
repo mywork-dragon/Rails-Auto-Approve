59467ced7511ab4b08d7e29d6c8ad68e440a37e9
mywork-dragon <dragonblueyounger@gmail.com>
2021-01-27 18:41:30 +0000

    Create initial JS and CSS file for Get a Quote Theme

---
+++ app/javascript/packs/get-a-quote.js
@@ -0,0 +1,25 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+
+import "stylesheets/get-a-quote";
+require("../scr/themes/modernizr")
+
+global.$ = require("../scr/themes/jquery.min")
+require("../scr/themes/material.min")
+require("../scr/themes/mdl-selectfield.min")
+// require("../scr/themes/getmdl-select) //this is being imported from public, as script
+require("../scr/themes/rangeslider.min")
+require("../scr/themes/range-slider")
+require("../scr/themes/jquery.collapsable")
+require("../scr/themes/footer")
+require("../scr/themes/flatpickr.min")
+import "../scr/flatpickr"
+require("../scr/themes/step-quote-custom")
+require("../scr/themes/custom")
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
