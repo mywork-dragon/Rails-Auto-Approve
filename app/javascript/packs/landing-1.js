79cffc8e8cb3b3bcb705ffd3032eb1a1ca669985
mywork-dragon <dragonblueyounger@gmail.com>
2021-01-27 15:37:36 +0000

    Set a separeted JS/CSS import. Not get from NPM packages, but from template

---
+++ app/javascript/packs/landing-1.js
@@ -0,0 +1,27 @@
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+
+import "stylesheets/landing-1";
+
+global.$ = require("../scr/themes/jquery.min")
+
+require("../scr/themes/material.min")
+require("../scr/themes/rangeslider.min")
+require("../scr/themes/flatpickr.min")
+require("../scr/themes/owl.carousel.min")
+require("../scr/themes/range-slider")
+require("../scr/themes/jquery.collapsable")
+require("../scr/themes/jquery.sweet-dropdown.min")
+require("../scr/themes/footer")
+require("../scr/themes/custom")
+require("../scr/themes/get-owl-slider")
+require("../scr/themes/step-quote-custom")
+import "../scr/flatpickr"
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
--- app/javascript/packs/themes/landing-1.js
+++
@@ -1,6 +0,0 @@
-import "../../stylesheets/themes/landing-1.scss";
-import "../../scr/flatpickr"
-
-global.getmdlSelect = require("../../scr/getmdl-select");
-require("../../scr/step-quote-custom")
-require("material-design-lite/material.min.js");
