15582c4d51729be51be477778742c2523b3a8da4
mywork-dragon <dragonblueyounger@gmail.com>
2021-01-28 14:03:28 +0000

    Redirec user to index after save, instead of show page. (show pages arent styled)

--- app/controllers/admin/faq_controller.rb
+++ app/controllers/admin/faq_controller.rb
@@ -15,7 +15,7 @@ class Admin::FaqController < AdminController
     @faq = Faq.new(position_params)
     if @faq.save
       flash[:notice] = 'Faq created successfully'
-      redirect_to admin_faq_path(@faq)
+      redirect_to admin_faq_index_path
     else
       render action: :new
     end
@@ -29,7 +29,7 @@ class Admin::FaqController < AdminController
     @faq = Faq.find(params[:id])
     if @faq.update(position_params)
       flash[:notice] = 'Faq updated successfully'
-      redirect_to admin_faq_path(@faq)
+      redirect_to admin_faq_index_path
     else
       render action: :edit
     end
