aca4c26b8d5ac61b507801c39079e97d5a1a6208
mywork-dragon <dragonblueyounger@gmail.com>
2021-01-28 14:33:18 +0000

    Remove location field from Reviews

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -47,7 +47,7 @@ class Admin::ReviewsController < AdminController
 
   def review_params
     params.require(:review).permit(
-      :review_site_id, :name, :content, :location, :url, :rating
+      :review_site_id, :name, :content, :url, :rating
     )
   end
 end
