02eb7e1d6c89b7501c097cf0f401a6e13cee7227
mywork-dragon <dragonblueyounger@gmail.com>
2021-03-25 20:49:47 +0000

    ! Remove css not used

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,31 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
-
 
 import "stylesheets/application"
 
-//global.$ = require("jquery")
-
-require("jquery.collapsable/dist/jquery.collapsable")
-//import "rangeslider.js/dist/rangeslider"
-//import "rangeslider.js/dist/rangeslider.css"
-import owlCarousel from "owl.carousel";
-import "owl.carousel/dist/assets/owl.carousel.min.css";
-
-import "../scr/get-owl-slider"
-//import "../scr/range-slider"
-
-//import "../scr/footer"
-
-
-//NEW
 
 import "../scr/custom"
 import "../scr/header"
-
-//import "../scr/jquery.sweet-dropdown.min"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/scr/footer.js
+++
@@ -1,7 +0,0 @@
-// $(document).on('turbolinks:load', function () {
-$(document).ready(function () {
-  $(".collapsable-slide").collapsable({
-    fx: "slide",
-    fxDuration: 300,
-  });
-});
