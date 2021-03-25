0e12fa8b338847d720470b6d3d3a48f6cc4b3865
mywork-dragon <dragonblueyounger@gmail.com>
2021-03-25 14:57:59 +0000

    + Add: Privacy Policy SLIM

--- app/javascript/scr/custom.js
+++ app/javascript/scr/custom.js
@@ -1,20 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  new Splide('.splide', {
-    type: 'loop',
-    perPage: 3,
-    autoplay: true,
-    pagination: false,
-    arrows: false,
-    interval: 4000,
-    speed: 4000,
-    waitForTransition: false,
-    perMove: 1,
-    gap: 32,
-    breakpoints: {
-      1280: { perPage: 2, gap: 24 },
-      840: { perPage: 1, gap: 16 },
-    },
-  }).mount();
+  
   window.addEventListener('scroll', handleScroll);
 });
 
