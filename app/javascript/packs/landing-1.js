96c28540c48af62924c298e56657de3e2bcea17d
mywork-dragon <dragonblueyounger@gmail.com>
2021-04-01 01:26:16 +0100

    ! FIX: import landing-1.js

--- app/javascript/packs/landing-1.js
+++ app/javascript/packs/landing-1.js
@@ -1,9 +1,10 @@
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 
 import "stylesheets/landing-1";
 
+import "../scr/custom"
+import "../scr/header"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
