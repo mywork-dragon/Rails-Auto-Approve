0e3d41e00b3082100a939d51ee55a28cc8ba0c8f
mywork-dragon <dragonblueyounger@gmail.com>
2021-04-10 00:35:46 +0100

    ! FIX: Logo not view active

--- app/javascript/packs/admin_scripts/review.js
+++ app/javascript/packs/admin_scripts/review.js
@@ -4,8 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
   if(inputRating){
     document.getElementById(`star-${inputRating}`).checked = true
   }
+
+  let a = ''
+  var labels = document.getElementsByTagName('label');
+  document.getElementsByName("review[review_site_id]").forEach((input) => {
+    if(input.checked){
+      a =  input;
+      for (var i = 0; i < labels.length; i++) {
+        if(labels[i].htmlFor == input.id){
+          labels[i].classList.add('active')
+        }
+        
+      }
+    }
+    input.addEventListener('change', changeLogo);
+  });
 })
 
 window.handleStar = function (radio) {
   document.getElementById("review_rating").value = radio.value
+}
+
+
+function changeLogo(event) {
+  document.getElementsByClassName("active")[0]?.classList.remove("active");
+  event.path[1].classList.add('active');
 }
