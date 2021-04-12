e355b983bb83f24f21be0fd0e1e168d11b867bf4
mywork-dragon <dragonblueyounger@gmail.com>
2021-04-12 18:05:09 +0100

    !FIX: Slug without /

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,12 +3,11 @@ class Resource < ApplicationRecord
   belongs_to :resource_category
   
   belongs_to :user
-
   mount_uploader :cover, CoverUploader
   enum state: { pedding: 0, live: 1, archived: 2 }
   
   validates :title, length: { maximum: 100 }, presence: true
   validates :subTitle, length: { maximum: 100 }, presence: true
   validates :content, :cover, presence: true
-  validates :slug, length: { maximum: 255 }, presence: true
+  validates :slug, length: { maximum: 255 }, format: { without: /\A\// }, presence: true
 end
