dc5430287e5629f3419a8d7254b98114547158f0
mywork-dragon <dragonblueyounger@gmail.com>
2021-04-12 22:12:49 +0100

    ! FIX: Switch live line.

--- app/models/resource_category.rb
+++ app/models/resource_category.rb
@@ -8,5 +8,5 @@ class ResourceCategory < ApplicationRecord
   enum state: { pedding: 0, live: 1, archived: 2 }
 
   validates :name, length: { maximum: 50 }, presence: true
-  validates :slug, length: { maximum: 255 }, presence: true
+  validates :slug, length: { maximum: 255 }, format: { without: /\A\// }, presence: true
 end
